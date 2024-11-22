6ac72c7c981bc81c82a149a6d170be87859a0c4a
Shamaun Nabi Ashik <snashik950@gmail.com>
2024-11-22 11:13:00 +0600

    Payment Method

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,8 +1,71 @@
 import React, { useContext } from 'react'
 import { CartContext } from '../context/CartContextt';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Checkout() {
   const {cart}= useContext(CartContext);
+
+  const navigate= useNavigate();
+
+ 
+  function calculateSubtotal() {
+    let subtotal = 0;
+    cart?.forEach((item) => {
+      subtotal =subtotal + item?.price;
+    });
+    return subtotal.toFixed(2);
+  }
+
+
+  // async function PayNow(){
+  //   let userPayAbleAmount= calculateSubtotal();
+
+  
+  //     try {
+  // const response= await axios.post("https://​sandbox​.aamarpay.com/jsonpost.php",amarPayJSON);
+  // const result= response.data;
+  // console.log(result);
+    
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  // }
+
+  async function PayNow() {
+    const userPayAbleAmount = calculateSubtotal();
+    const amarPayJSON = {
+  "store_id": "aamarpaytest",
+  "tran_id": "hasdhjasdhkashdsdf",
+  "success_url": "http://www.merchantdomain.com/suc esspage.html",
+  "fail_url": "http://www.merchantdomain.com/faile dpage.html",
+  "cancel_url": "http://www.merchantdomain.com/can cellpage.html",
+  "amount": userPayAbleAmount,
+  "currency": "BDT",
+  "signature_key": "dbb74894e82415a2f7ff0ec3a97e4183",
+  "desc": "Merchant Registration Payment",
+  "cus_name": "Name",
+  "cus_email": "payer@merchantcusomter.com",
+  "cus_add1": "House B-158 Road 22",
+  "cus_add2": "Mohakhali DOHS",
+  "cus_city": "Dhaka",
+  "cus_state": "Dhaka",
+  "cus_postcode": "1206",
+  "cus_country": "Bangladesh",
+  "cus_phone": "+8801704",
+  "type": "json"
+}
+
+    const response = await axios.post("http://localhost:3000/paynow", amarPayJSON);
+    console.log(response.data);
+   if (response?.data) {
+    window.location.href = response?.data;
+  } else {
+    console.error('Payment URL is not present in the response');
+  }
+}
+
 
   return (
     <>
@@ -35,6 +98,10 @@ export default function Checkout() {
             ))}
           </tbody>
         </table>
+        <div className='mt-8 text-right'>
+          <h3 className='text-2xl font-bold'>Subtotal: {calculateSubtotal()} BDT</h3>
+          <button onClick={PayNow} className='btn btn-success my-3'>Pay Now</button>
+        </div>
       </div>
     </div>
     </>
